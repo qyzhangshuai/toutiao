a2cdc3708306da14c7e7ae7624cf7e23e37aad9f
qyzhangshuai <2868227448@qq.com>
2019-08-02 17:16:36 +0800

    登录页面初次完成

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 
+// 引入axios
+import axios from 'axios'
 // 引入公共样式
 import '@/style/reset.min.css'
+Vue.prototype.$http = axios
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -2,12 +2,94 @@
   <div class="loginbox">
     <el-card class="box-card">
       <img src="../../assets/images/logo_index.png" alt />
+      <!-- 表单验证部分 -->
+      <el-form
+        :model="loginForm"
+        :rules="loginRules"
+        status-icon
+        ref="loginForm"
+        class="demo-ruleForm"
+      >
+        <el-form-item prop="mobile">
+          <el-input
+            v-model="loginForm.mobile"
+            style=" margin-top:10px"
+            autocomplete="on"
+            placeholder="请输入手机号"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            style="width:232px; margin-right:10px"
+            autocomplete="off"
+            placeholder="请输入验证码"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-checkbox v-model="checked" style="float:left">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        <el-button
+          type="primary"
+          :disabled="!checked"
+          style="width:100%; margin-top:10px"
+          @click="submit('loginForm')"
+        >登录</el-button>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    var validateMobile = (rule, value, callback) => {
+      const reg = /^1[3-9]\d{9}/g
+      if (!reg.test(value)) {
+        callback(new Error('手机格式输入不正确'))
+      } else {
+        callback()
+      }
+    }
+
+    return {
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      checked: true,
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: validateMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { min: 6, max: 6, message: '验证码格式不正确', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submit (formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          // 发送请求
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+              this.loginForm
+            )
+            .then(result => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('输入的手机号或者验证码不正确')
+            })
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -22,7 +104,7 @@ export default {}
     top: 50%;
     left: 50%;
     transform: translate(-50%, -60%);
-    width: 300px;
+    width: 400px;
     height: 300px;
     text-align: center;
     img {
